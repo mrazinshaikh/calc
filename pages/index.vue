9419a13a4944d2ab0a7cda86b15e53b830ec115d
mrazinshaikh <razinshaikh8732@gmail.com>
2022-07-11 12:25:16 +0530

    usd to inr added

--- pages/index.vue
+++ pages/index.vue
@@ -26,6 +26,12 @@
             class="form-control"
             id="basic-url"
             v-model="calculate.amount"
+            autofocus="autofocus"
+            @keyup.enter="
+              (e) => {
+                e.target.blur();
+              }
+            "
           />
         </div>
       </div>
@@ -33,16 +39,45 @@
 
     <div class="col-md-12">
       <label for="">
-        From {{ calculate.currency }} to {{ calculate.to }}
+        From {{ calculate.currency }} to {{ calculate.to }} Price[{{
+          calculate.toAmount
+        }}]
       </label>
     </div>
     <div class="col-md-12">
       <table class="table table-responsive">
         <tbody>
           <tr>
+            <td></td>
+            <td>{{ calculate.currency }}</td>
+            <td>{{ calculate.to }}</td>
+          </tr>
+          <tr>
+            <td class="w-25">Per Hour</td>
+            <td class="">{{ formatCurrency(perHour()) }}</td>
+            <td class="">{{ formatCurrency(perHour(true), true) }}</td>
+          </tr>
+          <tr>
             <td class="w-25">Per Day</td>
-            <td class="">{{ calculate.amount * 8 }}</td>
-            <td class="">{{ calculate.amount * 8 * 78 }}</td>
+            <td class="">{{ formatCurrency(perDay()) }}</td>
+            <td class="">{{ formatCurrency(perDay(true), true) }}</td>
+          </tr>
+          <tr>
+            <td class="w-25">Per Week</td>
+            <td class="">{{ formatCurrency(perWeek()) }}</td>
+            <td class="">{{ formatCurrency(perWeek(true), true) }}</td>
+          </tr>
+          <tr>
+            <td class="w-25">Per Month</td>
+            <td class="">{{ formatCurrency(perMonth()) }}</td>
+            <td class="">{{ formatCurrency(perMonth(true), true) }}</td>
+          </tr>
+          <tr>
+            <td class="w-25">Per Year</td>
+            <td class="">{{ formatCurrency(perYear()) }}</td>
+            <td class="">
+              {{ formatCurrency(perYear(true), true) }}
+            </td>
           </tr>
         </tbody>
       </table>
@@ -54,13 +89,19 @@ import { ref, onMounted } from "vue";
 import countryCodes from "../public/currencies";
 export default {
   setup() {
+    useHead({
+      title: "Calculate",
+      //   script: [{ src: "https://code.jquery.com/jquery-3.6.0.min.js" }],
+    });
     const currencies = ref(countryCodes);
     const calculate = reactive({
       currency: "USD",
       to: "INR",
       amount: null,
+      toAmount: 79.290504,
     });
     onMounted(async () => {
+      // getConvertToAmount();
       //   const url = await $fetch("https://api.apilayer.com/currency_data/list", {
       //     method: "GET",
       //     headers: {
@@ -73,9 +114,91 @@ export default {
       //     },
       //   });
     });
+
+    async function getConvertToAmount() {
+      if (calculate.currency == "" && calculate.to == "") {
+        return false;
+      }
+      const url = await $fetch(
+        `https://api.apilayer.com/currency_data/convert?to=${calculate.to}&from=${calculate.currency}&amount=1`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            //   "Access-Control-Allow-Origin": "*",
+            apikey: "",
+          },
+          async onResponse({ request, response, options }) {
+            if (response._data?.success) {
+              calculate.toAmount = response._data?.result;
+            }
+          },
+        }
+      );
+    }
+
+    function formatCurrency(value, to = false) {
+      if (value == "" || value == null) {
+        return value;
+      }
+      if (!to) {
+        return value.toLocaleString("en-US", {
+          maximumFractionDigits: 2,
+          style: "currency",
+          currency: "USD",
+        });
+      }
+      return value.toLocaleString("en-IN", {
+        maximumFractionDigits: 2,
+        style: "currency",
+        currency: "INR",
+      });
+    }
+
+    function perHour(to = false) {
+      if (!to) {
+        return calculate.amount;
+      }
+      return calculate.amount * calculate.toAmount;
+    }
+
+    function perDay(to = false) {
+      if (!to) {
+        return calculate.amount * 8;
+      }
+      return calculate.amount * 8 * calculate.toAmount;
+    }
+
+    function perWeek(to = false) {
+      if (!to) {
+        return perDay() * 5;
+      }
+      return perDay(true) * 5;
+    }
+
+    function perMonth(to = false) {
+      if (!to) {
+        return perWeek() * 4;
+      }
+      return perWeek(true) * 4;
+    }
+
+    function perYear(to = false) {
+      if (!to) {
+        return perMonth() * 12;
+      }
+      return perMonth(true) * 12;
+    }
+
     return {
       currencies,
       calculate,
+      formatCurrency,
+      perDay,
+      perWeek,
+      perHour,
+      perMonth,
+      perYear,
     };
   },
 };
